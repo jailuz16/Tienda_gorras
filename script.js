6012287a9e848550699003139210f5fc79a6693f
jailuz16 <cjailuz@gmail.com>
2025-03-06 11:28:26 -0500

    Primer comit

---
+++ script.js
@@ -0,0 +1,7 @@
+//const btn = document.getElementById("btn");
+//const container = document.querySelector(".container");
+
+//btn.addEventListener("click",()=>{
+ //   container.classList.toggle("toggle");
+
+//});
